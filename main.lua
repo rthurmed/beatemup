db2a2a0d57b556879e1ea15cef797409926f87f6
Arthur Medeiros <rthurmedeiros@gmail.com>
2020-09-07 19:48:11 -0300

    Screen debug

--- main.lua
+++ main.lua
@@ -1,9 +1,9 @@
 Util = require('util')
 
+SCALE_FACTOR = 40
+
 function love.load()
-  love.physics.setMeter(32)
   love.keyboard.setKeyRepeat(true)
-  World = love.physics.newWorld(0, 9.81*32, true)
 
   Animation = Util.newAnimation(love.graphics.newImage("assets/dog-run.png"), 64, 96, 1 / 3)
   Standing = love.graphics.newImage("assets/dog-stand.png")
@@ -20,7 +20,7 @@ function love.keypressed(key)
 end
 
 function love.keyreleased(key)
-  if key == "escape" then
+  if key == 'escape' then
     love.event.quit()
     return
   end
@@ -28,20 +28,18 @@ function love.keyreleased(key)
 end
 
 function love.update(dt)
-  World:update(dt)
-
   local originalX = PositionX
   local originalY = PositionY
 
   for key, value in pairs(Keys) do
     if value == true then
-      if key == "up" then
+      if key == 'up' then
         PositionY = PositionY - MoveSpeed
-      elseif key == "down" then
+      elseif key == 'down' then
         PositionY = PositionY + MoveSpeed
-      elseif key == "left" then
+      elseif key == 'left' then
         PositionX = PositionX - MoveSpeed
-      elseif key == "right" then
+      elseif key == 'right' then
         PositionX = PositionX + MoveSpeed
       end
     end
@@ -58,8 +56,14 @@ end
 function love.draw()
   if IsWalking then
     local spriteNum = math.floor(Animation.currentTime / Animation.duration * #Animation.quads) + 1
-    love.graphics.draw(Animation.spriteSheet, Animation.quads[spriteNum], PositionX, PositionY, 0, PositionY / 30)
+    love.graphics.draw(Animation.spriteSheet, Animation.quads[spriteNum], PositionX, PositionY, 0, PositionY / SCALE_FACTOR)
   else
-    love.graphics.draw(Standing, PositionX, PositionY, 0, PositionY / 30)
+    love.graphics.draw(Standing, PositionX, PositionY, 0, PositionY / SCALE_FACTOR)
   end
+
+  love.graphics.rectangle('line', PositionX, PositionY, 64 * PositionY / SCALE_FACTOR, 96 * PositionY / SCALE_FACTOR)
+
+  love.graphics.print('PositionX: ' .. PositionX, 0, 0)
+  love.graphics.print('PositionY: ' .. PositionY, 0, 15)
+  love.graphics.print('RealY: ' .. PositionY * PositionY / SCALE_FACTOR, 0, 30)
 end
